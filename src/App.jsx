802223e9ca0c9167dd9a6ed5e239869c62e5ea1e
unknown <nechetov@hotmail.com>
2021-04-14 14:09:15 +0300

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,6 @@ const App = () => {
       <Header />
       <div style={{ backgroundColor: 'red', height: '800px' }}></div>
       <div style={{ backgroundColor: 'blue', height: '800px' }}></div>
-      <div style={{ backgroundColor: 'green', height: '800px' }}></div>
-      <div style={{ backgroundColor: 'yellow', height: '800px' }}></div>
       <Footer />
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Fade } from 'react-awesome-reveal';
 
 const Footer = () => {
   return (
-    <div>
-      <div className="">Footer</div>
-    </div>
+    <footer className="bck_red">
+      <Fade triggerOnce={false} delay={200}>
+        <div className="font_righteous footer_logo_venue">The venue</div>
+        <div className="footer_copyright">The venue 2021 all rights reserved</div>
+      </Fade>
+    </footer>
   );
 };
 
