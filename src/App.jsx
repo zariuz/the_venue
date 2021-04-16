543e719eea55539136966c9d5dcf74bba84b8d30
unknown <nechetov@hotmail.com>
2021-04-16 11:38:27 +0300

    add carrousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Featured from './components/Featured';
 import './styles.css';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Featured />
       <div style={{ backgroundColor: 'red', height: '800px' }}></div>
       <div style={{ backgroundColor: 'blue', height: '800px' }}></div>
       <Footer />
